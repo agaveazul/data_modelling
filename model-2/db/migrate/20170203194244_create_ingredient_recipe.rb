bb05ca6e6a2ece9213595299924c03459526c6e4
Richard Strauss <rstrauss91@gmail.com>
2017-02-03 16:10:42 -0500

    create rails apps for models

---
+++ model-2/db/migrate/20170203194244_create_ingredient_recipe.rb
@@ -0,0 +1,8 @@
+class CreateIngredientRecipe < ActiveRecord::Migration[5.0]
+  def change
+    create_table :ingredients_recipes do |t|
+      t.integer :recipe_id
+      t.integer :ingredient_id
+    end
+  end
+end
---
+++ model-2/db/migrate/20170203195701_add_records_to_ingredient_recipe.rb
@@ -0,0 +1,5 @@
+class AddRecordsToIngredientRecipe < ActiveRecord::Migration[5.0]
+  def change
+    
+  end
+end
